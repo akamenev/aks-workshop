83c77d8337626cc23dbf220120c0c6a8d60cc270
Andrei Kamenev <ankamene@microsoft.com>
2019-07-03 16:48:43 +0300

    Added welcome-app files

---
+++ welcome-app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type Welcome struct {
+	Name string
+	Time string
+}
+
+
+func main() {
+
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+
+	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+	http.Handle("/static/", 
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static")))) 
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("Listening on Port 8080")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
